5bbdb80ba07b9c946e03593f6f1fbd112aaac35f
jonel.recto <jonel.recto@gmail.com>
2020-10-31 16:17:35 +0800

    first

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h2>this is also with folder users with {{$route.params.id}}</h2>
+    <ul v-for="(todo, i) in todos" :key="i">
+      <li>{{todo.desc}}</li>
+      <button @click="updateTodo(todo, i)" ></button>
+    </ul>
+
+    <input type="text" v-model="toUpdateDesc" />
+    <button @click="save" ></button>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      toUpdateTodo: {},
+      toUpdateDesc: '',
+      todos: [
+        { desc: 'hehe', isDone: false },
+        { desc: 'g', isDone: false },
+        { desc: 'ss', isDone: false },
+        { desc: 'tt', isDone: false }
+      ]
+    }
+  },
+  methods: {
+    updateTodo (todo, index) {
+      // console.log(todo, index,' todo')
+      this.toUpdateDesc = todo.desc
+      this.toUpdateTodo = { ...todo, index}
+    },
+    save() {
+      console.log(this.toUpdateTodo, this.toUpdateDesc, 'asf')
+      let desc = this.toUpdateDesc
+      
+      this.todos[this.toUpdateTodo.index] = { desc: this.toUpdateDesc, isDone: this.toUpdateTodo.isDone }
+      console.log(this.todos)
+      this.$forceUpdate()
+    }
+  }
+}
+</script>
